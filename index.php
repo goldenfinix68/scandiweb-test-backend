6180f2f4ad100be23d0aa67fe8ca45cb68d56b06
Golden Phoenix <goldenfinix@outlook.com>
2024-08-26 02:31:49 -0400

    Add base code

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require_once __DIR__ . '/vendor/autoload.php';
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+use App\App;
+
+App::run();
